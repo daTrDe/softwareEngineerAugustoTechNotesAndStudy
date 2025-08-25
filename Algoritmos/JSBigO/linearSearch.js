2fbb03d198df0c234e3e0d7779bbfee27202cc91
eragonAugusto <datd_94@live.com.mx>
2025-08-24 20:53:06 -0600

    Evaluación de complejidad temporal, espacial y auxiliar en ejercicios de js

--- Algoritmos/JSBigO/linearSearch.js
+++ Algoritmos/JSBigO/linearSearch.js
@@ -1,11 +1,11 @@
 /**
  * Complejidad Temporal -> O( n )
- * Complejidad Espacial -> O(  )
- * Espacio Auxiliar -> O(  )
+ * Complejidad Espacial -> O( n )
+ * Espacio Auxiliar -> Complejidad espacial - espacio de entrada = O( n ) - O( 1 ) = O( 1 )
  */
-function linearSearch(arreglo, clave) {
-  for (let indice = 0; indice < arreglo.length; indice++) { // O(n)
-    if (arreglo[indice] === clave) { // O(1)
+function linearSearch(arreglo, clave) { // O(n)
+  for (let indice = 0; indice < arreglo.length; indice++) { // O(n) // O(1)
+    if (arreglo[indice] === clave) { // O(1) //
       return indice; // O(1)
     }
   }
